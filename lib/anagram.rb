bb5a2996e828b6b8ac2384dfbb28542e2cf67b35
Dominique Skaf <doskaf@yahoo.com>
2019-09-22 23:45:11 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,7 +8,7 @@ class Anagram
   
   def match(array)
     %w(array).select do |element|
-      element.split("").sort == @word.split("").sort
+      (element.split("").sort) == (@word.split("").sort)
     end
   end
   
